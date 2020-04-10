87181efc09a473c70786c52ce1dd906e05368f00
HENRY-KC <henrykc24@yahoo.com>
2020-04-10 21:40:32 +0100

    worked on challenge 2

--- src/estimator.js
+++ src/estimator.js
@@ -22,7 +22,7 @@ const covid19ImpactEstimator = (data) => {
   const normalTTE = Math.floor(inDays(data.periodType, data.timeToElapse));
   const impactInfByRT = Math.floor(infByReqTime(normalTTE, impactRC));
   const sImpactInfByRT = Math.floor(infByReqTime(normalTTE, sImpactRC));
-  const impactSCByRT = Math.floor(0.15 * impactInfByRT);
+  const impactSCByRT = Math.floor(0.15 * impactInfByRT) + 1;
   const sImpactSCByRT = Math.floor(0.15 * sImpactInfByRT);
   const availableBeds = Math.floor(0.35 * data.totalHospitalBeds);
   const impactHBByRT = availableBeds - (Math.floor(0.15 * impactInfByRT));
