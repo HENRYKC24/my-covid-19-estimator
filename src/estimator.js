618ecc76500ab0e7ec54fc9c2257ce7b09d51f9a
HENRY-KC <henrykc24@yahoo.com>
2020-04-10 10:58:19 +0100

    corrected js lint errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,27 +1,18 @@
-
-/* a function that handles all vallues to determine how the infected 
-** persons doubles overtime
-*/
 const infectionsByRequestedTime = (elapsedTime, currentlyInfected) => {
-    //check multiples of 3 in the elapsed time
     const numOfThreesInElapsedTime = Math.floor(elapsedTime / 3);
-    //check the remainder after getting multiples of 3
     const extraDays = elapsedTime % 3;
-    //compute both
     const part1CurrentlyInfected = currentlyInfected * (2 ** numOfThreesInElapsedTime);
     const part2CurrentlyInfected = (part1CurrentlyInfected / 72) * extraDays;
     return Math.round(part1CurrentlyInfected + part2CurrentlyInfected);
 };
-
-//a function that normalises weeks and months to days
 const normaliseIndays = (periodType, timeToElapse) => {
     let result = timeToElapse;
-    const options = ["days", "weeks", "months"];
-    if (options.indexOf(periodType) == 1) {
+    const options = ['days', 'weeks', 'months'];
+    if (options.indexOf(periodType) === 1) {
         result = timeToElapse * 7;
     };
     let then;
-    if (options.indexOf(periodType) == 2) {
+    if (options.indexOf(periodType) === 2) {
         const now = new Date();
         const nowMonth = now.getMonth();
         const nowDate = now.getDate();
@@ -30,21 +21,16 @@ const normaliseIndays = (periodType, timeToElapse) => {
         const nowMinutes = now.getMinutes();
         const nowSeconds = now.getSeconds();
         const nowmilliseconds = now.getMilliseconds();
-        // const ordinalMonth = nowMonth + 1;
-        //add the given months(timeToElapse) to the present month
         const totalMonths = nowMonth + timeToElapse;
         const workingMonth = totalMonths + 1;
         const months30s = [4, 6, 9, 11];
-        //get how many years in the future
         if (workingMonth > 12) {
             const addedYears = Math.floor(workingMonth / 12);
             const futureYear = nowYear + addedYears;
             const futureMonths = workingMonth % 12;
-            //takes care of future leap year
             if (futureYear % 4 !== 0 && futureMonths == 2 && nowDate > 28) {
                 nowDate = 28;
             };
-            //takes care of month 30 days and 31 days conflicts
             if (months30s.indexOf(futureMonths) > -1 && nowDate == 31) {
                 nowDate = 30;
             };
@@ -95,6 +81,6 @@ const covid19ImpactEstimator = (data) => {
             casesForVentilatorsByRequestedTime: Math.round(0.02 * severeImpactInfectionsByRequestedTime),
             dollarsInFlight: severImpactDollarsInFlight
         }
-    };
+    }
 };
 export default covid19ImpactEstimator;
