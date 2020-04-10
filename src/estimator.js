eadd4d578dfca5a4accaf31bf3cd4d5fd74dc8b3
HENRY-KC <henrykc24@yahoo.com>
2020-04-10 16:24:46 +0100

    third fixes

--- src/estimator.js
+++ src/estimator.js
@@ -1,44 +1,44 @@
-const infectionsByRequestedTime = (elapsedTime, currentlyInfected) => {
+const infByReqTime = (elapsedTime, cInf) => {
     const numOfThreesInElapsedTime = Math.floor(elapsedTime / 3);
     const extraDays = elapsedTime % 3;
-    const part1CurrentlyInfected = currentlyInfected * (2 ** numOfThreesInElapsedTime);
+    const part1CurrentlyInfected = cInf * (2 ** numOfThreesInElapsedTime);
     const part2CurrentlyInfected = (part1CurrentlyInfected / 72) * extraDays;
     return Math.round(part1CurrentlyInfected + part2CurrentlyInfected);
 };
-const normaliseIndays = (periodType, timeToElapse) => {
+const inDays = (periodType, timeToElapse) => {
     let result = timeToElapse;
     const options = ['days', 'weeks', 'months'];
     if (options.indexOf(periodType) === 1) {
         result = timeToElapse * 7;
-    };
+    }
     let then;
     if (options.indexOf(periodType) === 2) {
         const now = new Date();
-        let nowMonth = now.getMonth();
+        const nowMonth = now.getMonth();
         let nowDate = now.getDate();
-        let nowYear = now.getFullYear();
-        const nowHour = now.getHours();
-        const nowMinutes = now.getMinutes();
-        const nowSeconds = now.getSeconds();
-        const nowmilliseconds = now.getMilliseconds();
+        const nowYr = now.getFullYear();
+        const nowHr = now.getHours();
+        const nowMins = now.getMinutes();
+        const nowSecs = now.getSeconds();
+        const nowMilSecs = now.getMilliseconds();
         const totalMonths = nowMonth + timeToElapse;
         const workingMonth = totalMonths + 1;
         const months30s = [4, 6, 9, 11];
         if (workingMonth > 12) {
-            const addedYears = Math.floor(workingMonth / 12);
-            const futureYear = nowYear + addedYears;
-            const futureMonths = workingMonth % 12;
-            if (futureYear % 4 !== 0 && futureMonths === 2 && nowDate > 28) {
+            const addedYrs = Math.floor(workingMonth / 12);
+            const fYr = nowYr + addedYrs;
+            const fMonths = workingMonth % 12;
+            if (fYr % 4 !== 0 && fMonths === 2 && nowDate > 28) {
                 nowDate = 28;
-            };
-            if (months30s.indexOf(futureMonths) > -1 && nowDate === 31) {
+            }
+            if (months30s.indexOf(fMonths) > -1 && nowDate === 31) {
                 nowDate = 30;
-            };
-            then = new Date(futureYear, futureMonths, nowDate, nowHour, nowMinutes, nowSeconds, nowmilliseconds);
+            }
+            then = new Date(fYr, fMonths, nowDate, nowHr, nowMins, nowSecs, nowMilSecs);
         } else {
-            const futureYear = nowYear;
-            const futureMonths = nowMonth + timeToElapse;
-            then = new Date(futureYear, futureMonths, nowDate, nowHour, nowMinutes, nowSeconds, nowmilliseconds);
+            const fYr = nowYr;
+            const fMonths = nowMonth + timeToElapse;
+            then = new Date(fYr, fMonths, nowDate, nowHr, nowMins, nowSecs, nowMilSecs);
         }
         const timeDifferenceInMilliseconds = then.getTime() - now.getTime();
         result = Math.ceil(timeDifferenceInMilliseconds / 86400000);
@@ -47,49 +47,43 @@ const normaliseIndays = (periodType, timeToElapse) => {
 };
 
 const covid19ImpactEstimator = (data) => {
-    const impactReportedCases = data.reportedCases * 10;
-    const severeImpactReportedCases = data.reportedCases * 50;
-    const normalisedTimeToElapse = normaliseIndays(data.periodType, data.timeToElapse);
-    const impactInfectionsByRequestedTime =
-     infectionsByRequestedTime(normalisedTimeToElapse, impactReportedCases);
-    const severeImpactInfectionsByRequestedTime =
-     infectionsByRequestedTime(normalisedTimeToElapse, severeImpactReportedCases);
-    const impactSevereCasesByRequestedTime = Math.round(0.15 * impactInfectionsByRequestedTime);
-    const severeImpactSevereCasesByRequestedTime =
-     Math.round(0.15 * severeImpactInfectionsByRequestedTime);
+    const impactRC = data.reportedCases * 10;
+    const sImpactRC = data.reportedCases * 50;
+    const normalTTE = inDays(data.periodType, data.timeToElapse);
+    const impactInfByRT = infByReqTime(normalTTE, impactRC);
+    const sImpactInfByRT = infByReqTime(normalTTE, sImpactRC);
+    const impactSCByRT = Math.round(0.15 * impactInfByRT);
+    const sImpactSCByRT = Math.round(0.15 * sImpactInfByRT);
     const availableBeds = Math.round(0.35 * data.totalHospitalBeds);
-    const impactHospitalBedsByRequestedTime =  availableBeds -
-     (Math.round(0.15 * impactInfectionsByRequestedTime));
-    const severeImpactHospitalBedsByRequestedTime = availableBeds -
-     (Math.round(0.15 * severeImpactInfectionsByRequestedTime));
-    const impactCasesForICUByRequestedTime = Math.round(0.05 * impactInfectionsByRequestedTime);
-    const severImpactCasesForICUByRequestedTime =
-     Math.round(0.05 * severeImpactInfectionsByRequestedTime);
-    const impactDollarsInFlight = Math.floor(impactInfectionsByRequestedTime *
-         data.region.avgDailyIncomePopulation * data.timeToElapse);
-    const severImpactDollarsInFlight = Math.floor(severeImpactInfectionsByRequestedTime *
-         data.region.avgDailyIncomePopulation * data.timeToElapse);
-    const impactVentilators = Math.round(0.02 * impactInfectionsByRequestedTime);
-    const severeImpactVentilators = Math.round(0.02 * severeImpactInfectionsByRequestedTime);
+    const impactHBByRT =  availableBeds - (Math.round(0.15 * impactInfByRT));
+    const sImpactHBByRT = availableBeds - (Math.round(0.15 * sImpactInfByRT));
+    const impactCForICUByRT = Math.round(0.05 * impactInfByRT);
+    const sImpactCForICUByRT = Math.round(0.05 * sImpactInfByRT);
+    const myltp = impactInfByRT * data.region.avgDailyIncomePopulation;
+    const impactDInF = Math.floor(myltp * data.timeToElapse);
+    const multp2 = sImpactInfByRT * data.region.avgDailyIncomePopulation;
+    const severImpactDollarsInFlight = Math.floor(multp2 * data.timeToElapse);
+    const impactVent = Math.round(0.02 * impactInfByRT);
+    const sImpactVent = Math.round(0.02 * sImpactInfByRT);
     return {
         data: data,
         impact: {
-            currentlyInfected: impactReportedCases,
-            infectionsByRequestedTime: impactInfectionsByRequestedTime,
-            severeCasesByRequestedTime: impactSevereCasesByRequestedTime,
-            hospitalBedsByRequestedTime: impactHospitalBedsByRequestedTime,
-            casesForICUByRequestedTime: impactCasesForICUByRequestedTime,
-            casesForVentilatorsByRequestedTime: impactVentilators,
-            dollarsInFlight: impactDollarsInFlight
+            currentlyInfected: impactRC,
+            infectionsByRequestedTime: impactInfByRT,
+            severeCasesByRequestedTime: impactSCByRT,
+            hospitalBedsByRequestedTime: impactHBByRT,
+            casesForICUByRequestedTime: impactCForICUByRT,
+            casesForVentilatorsByRequestedTime: impactVent,
+            dollarsInFlight: impactDInF
         },
         severeImpact: {
-            currentlyInfected: severeImpactReportedCases,
-            infectionsByRequestedTime: severeImpactInfectionsByRequestedTime,
-            severeCasesByRequestedTime: severeImpactSevereCasesByRequestedTime,
-            hospitalBedsByRequestedTime: severeImpactHospitalBedsByRequestedTime,
-            casesForICUByRequestedTime: severImpactCasesForICUByRequestedTime,
-            casesForVentilatorsByRequestedTime: severeImpactVentilators,
-            dollarsInFlight: severImpactDollarsInFlight
+            currentlyInfected: sImpactRC,
+            infectionsByRequestedTime: sImpactInfByRT,
+            severeCasesByRequestedTime: sImpactSCByRT,
+            hospitalBedsByRequestedTime: sImpactHBByRT,
+            casesForICUByRequestedTime: sImpactCForICUByRT,
+            casesForVentilatorsByRequestedTime: sImpactVent,
+            dollarsInFlight: sImpactDInF
         }
     }
 };
