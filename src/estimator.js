6a37b6b220e589e9b46b25d26dd9f61ca6ddc9c0
HENRY-KC <henrykc24@yahoo.com>
2020-04-11 13:10:50 +0100

    corrected cases for ventilators

--- src/estimator.js
+++ src/estimator.js
@@ -16,22 +16,22 @@ const inDays = (periodType, timeToElapse) => {
 const covid19ImpactEstimator = (data) => {
   const impactRC = data.reportedCases * 10;
   const sImpactRC = data.reportedCases * 50;
-  const normalTTE = Math.trunc(inDays(data.periodType, data.timeToElapse), 10);
-  const impactInfByRT = Math.trunc(infByReqTime(normalTTE, impactRC), 10);
-  const sImpactInfByRT = Math.trunc(infByReqTime(normalTTE, sImpactRC), 10);
-  const impactSCByRT = Math.trunc(0.15 * impactInfByRT, 10);
-  const sImpactSCByRT = Math.trunc(0.15 * sImpactInfByRT, 10);
+  const normalTTE = Math.trunc(inDays(data.periodType, data.timeToElapse));
+  const impactInfByRT = Math.trunc(infByReqTime(normalTTE, impactRC));
+  const sImpactInfByRT = Math.trunc(infByReqTime(normalTTE, sImpactRC));
+  const impactSCByRT = Math.trunc(0.15 * impactInfByRT);
+  const sImpactSCByRT = Math.trunc(0.15 * sImpactInfByRT);
   const availableBeds = 0.35 * data.totalHospitalBeds;
-  const impactHBByRT = Math.trunc(availableBeds - (0.15 * impactInfByRT), 10);
-  const sImpactHBByRT = Math.trunc(availableBeds - (0.15 * sImpactInfByRT), 10);
-  const impactCForICUByRT = Math.trunc(0.05 * impactInfByRT, 10);
-  const sImpactCForICUByRT = Math.trunc(0.05 * sImpactInfByRT, 10);
-  const impactVent = Math.trunc((0.02 * impactInfByRT / 10), 10);
-  const sImpactVent = Math.trunc((0.02 * sImpactInfByRT / 50), 10);
+  const impactHBByRT = Math.trunc(availableBeds - (0.15 * impactInfByRT));
+  const sImpactHBByRT = Math.trunc(availableBeds - (0.15 * sImpactInfByRT));
+  const impactCForICUByRT = Math.trunc(0.05 * impactInfByRT);
+  const sImpactCForICUByRT = Math.trunc(0.05 * sImpactInfByRT);
+  const impactVent = Math.trunc((0.02 * impactInfByRT / 10));
+  const sImpactVent = Math.trunc((0.02 * sImpactInfByRT / 50));
   const myltp = impactInfByRT * data.region.avgDailyIncomePopulation;
-  const impactDInF = Math.trunc(myltp * normalTTE * data.region.avgDailyIncomeInUSD, 10);
+  const impactDInF = Math.trunc(myltp * normalTTE * data.region.avgDailyIncomeInUSD);
   const multp2 = sImpactInfByRT * data.region.avgDailyIncomePopulation;
-  const sImpactDInF = Math.trunc(multp2 * normalTTE * data.region.avgDailyIncomeInUSD, 10);
+  const sImpactDInF = Math.trunc(multp2 * normalTTE * data.region.avgDailyIncomeInUSD);
   return {
     data,
     impact: {
